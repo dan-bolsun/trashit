c06d23d6344927928ca042bb10da2edcd15953e6
Daniil Bolsun <dan.bolsun@gmail.com>
2025-09-19 07:32:55 +0300

    removed predefined adb.exe path, better usage explanation

--- script/run-android.ps1
+++ script/run-android.ps1
@@ -1,9 +1,6 @@
-
-# adb.exe pair 192.168.100.4:.....
-# adb.exe connect 192.168.100.4:.....
-
 if ($args[0]) {
-    node.exe node_modules\web-ext\bin\web-ext.js run --config=config.mjs --adb-bin=X:\tool\platform-tools\adb.exe --target=firefox-android --android-device=$args[0]
+    node.exe node_modules\web-ext\bin\web-ext.js run --config=config.mjs --target=firefox-android --android-device=$args[0]
 } else {
-    Write-Host -ForegroundColor red "Please specify ADB host:port"
+    Write-Host -ForegroundColor red "USAGE: run-android.ps1 <HOST:PORT>"
+    Write-Host -ForegroundColor yellow "HOST:PORT -- remote ADB device"
 }
